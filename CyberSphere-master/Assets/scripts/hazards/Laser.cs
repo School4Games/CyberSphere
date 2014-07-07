6c5d8db2adc92ed56c47834193e46fac2a7b7294
TheBigD <otakulp@gmx.de>
2014-07-07 10:20:16 +0200

    7.7
    - fused with Moritz
    - added rotation script

---
+++ CyberSphere-master/Assets/scripts/hazards/Laser.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	LineRenderer linerenderer;
+
+	public Transform beam;
+
+	public Transform sparks;
+
+	// Use this for initialization
+	void Start () {
+		linerenderer = beam.gameObject.GetComponent("LineRenderer") as LineRenderer;
+		beam.parent = null;
+		linerenderer.useWorldSpace = true;
+	}
+
+	void drawLine () {
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hitinfo = new RaycastHit ();
+		Physics.Raycast(ray, out hitinfo);
+		linerenderer.SetPosition (0, transform.position);
+		if (hitinfo.point != Vector3.zero) {
+			linerenderer.SetPosition (1, hitinfo.point);
+			sparks.position = hitinfo.point;
+			sparks.gameObject.SetActive(true);
+			
+		}
+		else {
+			linerenderer.SetPosition (1, transform.position + transform.forward * 1000);
+			sparks.gameObject.SetActive(false);
+		}
+		//damage sphere
+		/*GameObject sphere = hitinfo.collider.gameObject;
+		if (sphere.GetComponent("SphereController")) {
+			SphereController sphereController = sphere.GetComponent("SphereController") as SphereController;
+			GameObject graphics = sphereController.graphics.gameObject;
+			Color newColor = graphics.renderer.material.color;
+			newColor.r += Time.deltaTime * 10;
+			graphics.renderer.material.color = newColor;
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		drawLine ();
+	}
+}
---
+++ CyberSphere-master/Assets/scripts/player/TPCamera.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TPCamera : MonoBehaviour {
+
+	public Transform player;
+
+	public float XSensitivity = 100;
+	public float YSensitivity = 100;
+
+	//changes when spidershere activates
+	Vector3 upVector = Vector3.up;
+
+	//add more params for handling ... ask Tim
+
+	void follow () {
+		transform.position = player.position - transform.forward * 5;
+	}
+
+	void rotate () {
+		if (Input.GetAxis("Mouse X") != 0) {
+			transform.RotateAround (player.position, upVector, Input.GetAxis("Mouse X") * Time.deltaTime * XSensitivity);
+		}
+		if (Input.GetAxis("Mouse Y") != 0) {
+			transform.RotateAround (player.position, -transform.right, Input.GetAxis("Mouse Y") * Time.deltaTime * YSensitivity);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+		Screen.lockCursor = true;
+	}
+
+	void OnPreRender () {
+		rotate ();
+		follow ();
+	}
+
+	void checkUp () {
+		SphereController sphereController = player.GetComponent ("SphereController") as SphereController;
+		if (sphereController.spider && sphereController.adhesionForce != Vector3.zero) {
+			upVector = -sphereController.adhesionForce;
+			//if upside down
+			if (Vector3.Angle(transform.up, upVector) > 90) {
+				float angle0 = Vector3.Angle(transform.up, upVector);
+				int p0 = clockwisePrefix (transform.right, transform.up, upVector);
+				transform.RotateAround(transform.position, p0 * -transform.right, Mathf.Sqrt(angle0/10));
+			}
+			else {
+				float angle = Vector3.Angle(Vector3.Cross(transform.forward, upVector), Vector3.Cross(transform.forward, transform.up));
+				if (angle > 0 && upVector.normalized != transform.up && transform.forward != upVector.normalized) {
+					int p = clockwisePrefix (transform.forward, Vector3.Cross(transform.forward, upVector), Vector3.Cross(transform.forward, transform.up));
+					transform.RotateAround(transform.position, p * transform.forward, Mathf.Min(Mathf.Sqrt(angle/10), 2));
+				}
+			}
+		}
+		else if (!sphereController.spider && upVector.normalized != transform.up) {
+			upVector = Vector3.up;
+			//if upside down
+			if (Vector3.Angle(transform.up, upVector) > 90) {
+				float angle0 = Vector3.Angle(transform.up, upVector);
+				int p0 = clockwisePrefix (transform.right, transform.up, upVector);
+				transform.RotateAround(transform.position, p0 * -transform.right, Mathf.Sqrt(angle0/10));
+			}
+			else {
+				float angle = Vector3.Angle(Vector3.Cross(transform.forward, Vector3.up), Vector3.Cross(transform.forward, transform.up));
+				int p = clockwisePrefix (transform.forward, Vector3.Cross(transform.forward, Vector3.up), Vector3.Cross(transform.forward, transform.up));
+				transform.RotateAround(transform.position, p * transform.forward, Mathf.Sqrt(angle/10));
+			}
+		}
+	}
+
+	//returns 1 if clockwise, -1 if counterclockwise
+	//can you put stuff like this in some sort of library?
+	int clockwisePrefix (Vector3 axis, Vector3 direction, Vector3 targetDirection) {
+		if (Vector3.Angle(Vector3.Cross(targetDirection, direction), axis) < 90) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//broken for some reason xD
+		//rotates the camera
+		checkUp ();
+	}
+}
