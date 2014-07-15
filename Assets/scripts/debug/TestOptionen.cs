c71a92b0826a36d5ad0dca43650465bb09905f03
Lennart Daldrup <ldaldrup95@gmail.com>
2014-07-15 23:19:13 +0200

    Updated Options
    Added toggle-able Options.

--- Assets/scripts/debug/TestOptionen.cs
+++ Assets/scripts/debug/TestOptionen.cs
@@ -7,13 +7,26 @@ public class TestOptionen : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	bool isFullscreen;
+	bool guiEnable = false;
+
 	// Update is called once per frame
 	void Update () {
 		CheckFullscreen ();
+		EscapeToMenu ();
 	}
-
-	bool isFullscreen;
+	
+	void EscapeToMenu () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (guiEnable == false){
+				guiEnable = true;
+			}
+			else {
+				guiEnable = false;
+			}
+		}
+	}
 
 	void CheckFullscreen () {
 		if (Screen.fullScreen == true) {
@@ -28,42 +41,42 @@ public class TestOptionen : MonoBehaviour {
 	int TFrameInt = 60;
 
 	void OnGUI() { // Buttons for Resoltion Options / Values (new SHAPE (Left, Top, Width, Heigth), Text)
+		if (guiEnable == true) {
+						hSliderValue = GUI.HorizontalSlider (new Rect (10, 300, 100, 30), hSliderValue, 10.0F, 300.0F);
+						int TFrameInt = Mathf.RoundToInt (hSliderValue);
+						var TFrame = TFrameInt.ToString ();
+						GUI.Label (new Rect (130, 300, 100, 20), TFrame);
+						var abc = isFullscreen.ToString ();
+						GUI.Label (new Rect (170, 120, 100, 50), ("Fullscreen: " + abc));
 
-		hSliderValue = GUI.HorizontalSlider(new Rect(10, 300, 100, 30), hSliderValue, 10.0F, 300.0F);
-		int TFrameInt = Mathf.RoundToInt (hSliderValue);
-		var TFrame = TFrameInt.ToString ();
-		GUI.Label (new Rect (130, 300, 100, 20), TFrame);
-		var abc = isFullscreen.ToString();
-		GUI.Label (new Rect (170, 120, 100, 50), ("Fullscreen: " + abc));
 
 
+						if (GUI.Button (new Rect (10, 10, 150, 50), "1280x720 / 60Hz")) { // ResoBtn1
+								Debug.Log ("720p Windowed 144Hz"); 
+								Reso1 (); // Call function Reso1
+						}
 
-		if (GUI.Button (new Rect (10, 10, 150, 50), "1280x720 / 60Hz")) { // ResoBtn1
-			Debug.Log ("720p Windowed 144Hz"); 
-			Reso1 (); // Call function Reso1
-		}
-
-		if (GUI.Button (new Rect (10, 70, 150, 50), "1920x1080 / 60Hz")) { // ResoBtn2
-			Debug.Log ("1080p Windowed 144Hz");
-			Reso2(); // Call function Reso2
-		}
-		if (GUI.Button (new Rect (10, 130, 150, 50), "1600x900 / 60Hz")) { // ResoBtn3
-			Debug.Log ("900 Windowed 144Hz");
-			Reso3(); // Call function Reso3
-		}
-		if (GUI.Button (new Rect (10, 200, 150, 50), "Set Framerate to Slider")) {
-			TFramerate();
-		}
-		if (GUI.Button (new Rect (170, 10, 100, 50), "VSync On")) {
-			VSync1 ();
-		}
-		if (GUI.Button (new Rect (280, 10, 100, 50), "VSync Off")) {
-			VSync0 ();
-		}
-		if (GUI.Button (new Rect (170, 70, 100, 50), "Fullscreen")) {
-			FScreen();
+						if (GUI.Button (new Rect (10, 70, 150, 50), "1920x1080 / 60Hz")) { // ResoBtn2
+								Debug.Log ("1080p Windowed 144Hz");
+								Reso2 (); // Call function Reso2
+						}
+						if (GUI.Button (new Rect (10, 130, 150, 50), "1600x900 / 60Hz")) { // ResoBtn3
+								Debug.Log ("900 Windowed 144Hz");
+								Reso3 (); // Call function Reso3
+						}
+						if (GUI.Button (new Rect (10, 200, 150, 50), "Set Framerate to Slider")) {
+								TFramerate ();
+						}
+						if (GUI.Button (new Rect (170, 10, 100, 50), "VSync On")) {
+								VSync1 ();
+						}
+						if (GUI.Button (new Rect (280, 10, 100, 50), "VSync Off")) {
+								VSync0 ();
+						}
+						if (GUI.Button (new Rect (170, 70, 100, 50), "Fullscreen")) {
+								FScreen ();
+						}
 				}
-
 	}
 
 	void Reso1() {
