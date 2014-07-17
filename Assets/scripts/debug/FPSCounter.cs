98f832472777d9069f2d4170cf0a644d172f4ad6
Lennart Daldrup <ldaldrup95@gmail.com>
2014-07-17 17:30:39 +0200

    mini update

--- Assets/scripts/debug/FPSCounter.cs
+++ Assets/scripts/debug/FPSCounter.cs
@@ -21,42 +21,48 @@ public class FPSCounter : MonoBehaviour
 	private int   frames  = 0; // Frames drawn over the interval
 	private float timeleft; // Left time for current interval
 	
-	void Start()
-	{
-		if( !guiText )
-		{
+	void Start() {
+
+		if( !guiText ) {
 			Debug.Log("UtilityFramesPerSecond needs a GUIText component!");
 			enabled = false;
 			return;
 		}
 		timeleft = updateInterval;  
+
 	}
-	
-	void Update()
-	{
+
+	void Update() {
+			FPS ();
+		}
+
+	void FPS () {
 		timeleft -= Time.deltaTime;
-		accum += Time.timeScale/Time.deltaTime;
+		accum += Time.timeScale / Time.deltaTime;
 		++frames;
-		
+	
 		// Interval ended - update GUI text and start new interval
-		if( timeleft <= 0.0 )
-		{
+		if (timeleft <= 0.0) {
 			// display two fractional digits (f2 format)
-			float fps = accum/frames;
-			string format = System.String.Format("{0:F2} FPS",fps);
+			float fps = accum / frames;
+			string format = System.String.Format ("{0:F2} FPS", fps);
 			guiText.text = format;
-			
-			if(fps < 30)
-				guiText.material.color = Color.yellow;
-			else 
-				if(fps < 10)
+
+			if (fps < 30) {
+					guiText.material.color = Color.yellow;
+			}
+			else {
+				if (fps < 10) {
 					guiText.material.color = Color.red;
-			else
-				guiText.material.color = Color.green;
-			//	DebugConsole.Log(format,level);
-			timeleft = updateInterval;
-			accum = 0.0F;
-			frames = 0;
+				}
+				else {
+					guiText.material.color = Color.green;
+					//	DebugConsole.Log(format,level);
+					timeleft = updateInterval;
+					accum = 0.0F;
+					frames = 0;
+				}
+			}
 		}
 	}
 }
